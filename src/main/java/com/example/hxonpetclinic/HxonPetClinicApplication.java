2afdc3977a8ce1045107f1d748be00693a483661
Radek <rwprzybylowski@gmail.com>
2019-11-17 16:56:09 +0100

    init commit

---
+++ src/main/java/com/example/hxonpetclinic/HxonPetClinicApplication.java
@@ -0,0 +1,13 @@
+package com.example.hxonpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HxonPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HxonPetClinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/hxonpetclinic/HxonPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hxonpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HxonPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
