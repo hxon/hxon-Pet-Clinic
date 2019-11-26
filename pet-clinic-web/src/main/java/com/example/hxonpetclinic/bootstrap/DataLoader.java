a4f30648c8949a07c7dae6b0ba2373ce51916e89
hxon <haaxik1@gmail.com>
2019-11-26 10:36:30 +0100

    added PetType data to bootstrap. closes #32.

--- pet-clinic-web/src/main/java/com/example/hxonpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/example/hxonpetclinic/bootstrap/DataLoader.java
@@ -1,8 +1,10 @@
 package com.example.hxonpetclinic.bootstrap;
 
 import com.example.hxonpetclinic.model.Owner;
+import com.example.hxonpetclinic.model.PetType;
 import com.example.hxonpetclinic.model.Vet;
 import com.example.hxonpetclinic.services.OwnerService;
+import com.example.hxonpetclinic.services.PetTypeService;
 import com.example.hxonpetclinic.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -12,14 +14,24 @@ public class DataLoader implements CommandLineRunner {
 
     private final OwnerService ownerService;
     private final VetService vetService;
+    private final PetTypeService petTypeService;
 
-    public DataLoader(OwnerService ownerService, VetService vetService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
     }
 
     @Override
     public void run(String... args) throws Exception {
+
+        PetType dog = new PetType();
+        dog.setName("Dog");
+        PetType savedDogPetType = petTypeService.save(dog);
+
+        PetType cat = new PetType();
+        dog.setName("Cat");
+        PetType savedCatPetType = petTypeService.save(dog);
 
         Owner owner1 = new Owner();
         owner1.setFirstName("Michael");
