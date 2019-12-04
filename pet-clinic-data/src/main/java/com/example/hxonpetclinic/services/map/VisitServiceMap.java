ffd8fba24058eba267270d99cdb5b6026832bb27
hxon <haaxik1@gmail.com>
2019-12-04 13:59:48 +0100

    created visit jpa service, closes #47

--- pet-clinic-data/src/main/java/com/example/hxonpetclinic/services/map/VisitServiceMap.java
+++ pet-clinic-data/src/main/java/com/example/hxonpetclinic/services/map/VisitServiceMap.java
@@ -2,9 +2,13 @@ package com.example.hxonpetclinic.services.map;
 
 import com.example.hxonpetclinic.model.Visit;
 import com.example.hxonpetclinic.services.VisitService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
+@Service
+@Profile({"map", "default"})
 public class VisitServiceMap extends AbstractServiceMap<Visit, Long> implements VisitService {
 
 
