c02e84e5c4cf94e1680a43740b54491ab970d7bf
hxon <haaxik1@gmail.com>
2019-12-04 13:40:03 +0100

    Added Project Lombok and refactored, closes #51

--- pet-clinic-data/src/main/java/com/example/hxonpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/com/example/hxonpetclinic/model/Vet.java
@@ -1,9 +1,16 @@
 package com.example.hxonpetclinic.model;
 
+import lombok.*;
+
 import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Entity
 @Table(name = "vets")
 public class Vet extends Person {
@@ -18,7 +25,4 @@ public class Vet extends Person {
         return specialities;
     }
 
-    public void setSpecialities(Set<Speciality> specialities) {
-        this.specialities = specialities;
-    }
 }
